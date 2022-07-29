a35f69df81901f731bbd10fd7897bdcf96f41a4b
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2022-07-29 16:00:26 -0300

    style: indentation

--- ui.frontend/tests/unit/Button.spec.js
+++ ui.frontend/tests/unit/Button.spec.js
@@ -2,22 +2,22 @@ import { shallowMount } from '@vue/test-utils'
 import ButtonPedro from '@/components/micro/Button/Button.vue'
 
 describe('Button.vue', () => {
-    it('without props specified', () => {
-        const wrapper = shallowMount(ButtonPedro);
-        const buttonEl = wrapper.find('button').element;
-        expect(buttonEl.textContent).toBe('BACK TO HOMEPAGE');
-    });
-    it('with props specified', () => {
-        const wrapper = shallowMount(ButtonPedro, {
-            propsData: {
-                buttonType: 'primary',
-                buttonContent: 'return',
-                buttonFontSize: '14'
-            }
-        });
-        const buttonEl = wrapper.find('button').element;
-        expect(buttonEl.textContent).toBe('RETURN');
-        expect(buttonEl.classList[0]).toBe('primary');
-        expect(buttonEl.style.fontSize).toBe((14 / 16) + 'em');
-    });
+  it('without props specified', () => {
+    const wrapper = shallowMount(ButtonPedro)
+    const buttonEl = wrapper.find('button').element
+    expect(buttonEl.textContent).toBe('BACK TO HOMEPAGE')
+  })
+  it('with props specified', () => {
+    const wrapper = shallowMount(ButtonPedro, {
+      propsData: {
+        buttonType: 'primary',
+        buttonContent: 'return',
+        buttonFontSize: '14'
+      }
+    })
+    const buttonEl = wrapper.find('button').element
+    expect(buttonEl.textContent).toBe('RETURN')
+    expect(buttonEl.classList[0]).toBe('primary')
+    expect(buttonEl.style.fontSize).toBe((14 / 16) + 'em')
+  })
 })
--- ui.frontend/tests/unit/Title.spec.js
+++ ui.frontend/tests/unit/Title.spec.js
@@ -2,23 +2,23 @@ import { shallowMount } from '@vue/test-utils'
 import TitlePedro from '@/components/micro/Title/Title.vue'
 
 describe('Title.vue', () => {
-    it('without props specified', () => {
-        const wrapper = shallowMount(TitlePedro);
-        const titleEl = wrapper.find('h1').element;
-        expect(titleEl.textContent).toBe('I have bad news for you');
-    });
-    it('with props specified', () => {
-        const wrapper = shallowMount(TitlePedro, {
-            propsData: {
-                titleType: 'h1',
-                titleContent: 'Error 404',
-                titleColor: '#000000',
-                titleSize: '64'
-            }
-        });
-        const titleEl = wrapper.find('h1').element;
-        expect(titleEl.textContent).toBe('Error 404');
-        expect(titleEl.style.color).toBe('rgb(0, 0, 0)');
-        expect(titleEl.style.fontSize).toBe((64 / 16) + 'em');
-    });
+  it('without props specified', () => {
+    const wrapper = shallowMount(TitlePedro)
+    const titleEl = wrapper.find('h1').element
+    expect(titleEl.textContent).toBe('I have bad news for you')
+  })
+  it('with props specified', () => {
+    const wrapper = shallowMount(TitlePedro, {
+      propsData: {
+        titleType: 'h1',
+        titleContent: 'Error 404',
+        titleColor: '#000000',
+        titleSize: '64'
+      }
+    })
+    const titleEl = wrapper.find('h1').element
+    expect(titleEl.textContent).toBe('Error 404')
+    expect(titleEl.style.color).toBe('rgb(0, 0, 0)')
+    expect(titleEl.style.fontSize).toBe((64 / 16) + 'em')
+  })
 })
